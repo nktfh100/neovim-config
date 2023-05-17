29cce47f74d5b2fcab0cc56ce8d63aa2d30eb5e5
nktfh100 <me@nktfh100.com>
2023-05-17 11:02:58 +0300

    Trying to figure out LSP

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,41 +2,35 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-	"tsserver",
-	"eslint",
-	"jsonls",
-	"cssls",
-	"dockerls",
-	"docker_compose_language_service",
-	"gopls",
-	"html",
-	"lua_ls",
-	"marksman",
-	"pyright",
-	"sqlls",
-	"yamlls",
-})
-
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
+    lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
 end)
 
--- (Optional) Configure lua language server for neovim
 require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
 
-local null_ls = require("null-ls")
-local null_opts = lsp.build_options("null-ls", {})
+require("mason").setup()
 
-null_ls.setup({
-	on_attach = function(client, bufnr)
-		null_opts.on_attach(client, bufnr)
-	end,
-	sources = {
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.code_actions.xo,
-		null_ls.builtins.completion.spell,
-	},
+require("mason-null-ls").setup({
+    ensure_installed = {
+        "stylua",
+        "rome",
+        "black",
+        "gopls",
+        "lua-language-server",
+        "marksman",
+        "eslint",
+        "css-lsp",
+        "isort",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "yaml-language-server",
+        "pyright",
+        "python-lsp-server",
+        "cspell",
+    },
+    automatic_installation = true,
+    handlers = {},
 })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,156 +1,162 @@
 local vim = vim
 
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+        vim.cmd([[packadd packer.nvim]])
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+    use("wbthomason/packer.nvim")
 
-	-- Theme
-	-- use 'sainnhe/sonokai'
-	use("hardhackerlabs/theme-vim")
+    -- Theme
+    -- use 'sainnhe/sonokai'
+    use("hardhackerlabs/theme-vim")
 
-	-- Time tracking
-	use("wakatime/vim-wakatime")
+    -- Time tracking
+    use("wakatime/vim-wakatime")
 
-	use("nvim-tree/nvim-web-devicons")
+    use("nvim-tree/nvim-web-devicons")
 
-	use({
-		"folke/which-key.nvim",
-		config = function()
-			require("which-key").setup({})
-		end,
-	})
+    use({
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup({})
+        end,
+    })
 
-	-- Syntax highlighting
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		requires = {
-			"HiPhish/nvim-ts-rainbow2",
-		},
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-	})
+    -- Syntax highlighting
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        requires = {
+            "HiPhish/nvim-ts-rainbow2",
+        },
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
+        end,
+    })
 
-	-- File explorer
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = {
-			"nvim-tree/nvim-web-devicons",
-		},
-	})
+    -- File explorer
+    use({
+        "nvim-tree/nvim-tree.lua",
+        requires = {
+            "nvim-tree/nvim-web-devicons",
+        },
+    })
 
-	use({
-		"folke/trouble.nvim",
-		requires = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("trouble").setup({})
-		end,
-	})
+    use({
+        "folke/trouble.nvim",
+        requires = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("trouble").setup({})
+        end,
+    })
 
-	-- Fuzzy finder
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-		},
-	})
+    -- Fuzzy finder
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+        },
+    })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-		config = function()
-			require("lualine").setup()
-		end,
-	})
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
+        config = function()
+            require("lualine").setup()
+        end,
+    })
 
-	use({
-		"phaazon/hop.nvim",
-		branch = "v2",
-		config = function()
-			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-		end,
-	})
+    use({
+        "phaazon/hop.nvim",
+        branch = "v2",
+        config = function()
+            require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+        end,
+    })
 
-	use({
-		"terrortylor/nvim-comment",
-		config = function()
-			require("nvim_comment").setup()
-		end,
-	})
+    use({
+        "terrortylor/nvim-comment",
+        config = function()
+            require("nvim_comment").setup()
+        end,
+    })
 
-	use({
-		"antosha417/nvim-lsp-file-operations",
-		requires = {
-			{ "nvim-lua/plenary.nvim" },
-		},
-	})
+    use({
+        "antosha417/nvim-lsp-file-operations",
+        requires = {
+            { "nvim-lua/plenary.nvim" },
+        },
+    })
 
-	-- Greeter
-	use({
-		"goolord/alpha-nvim",
-		requires = { "nvim-tree/nvim-web-devicons" },
-	})
+    -- Greeter
+    use({
+        "goolord/alpha-nvim",
+        requires = { "nvim-tree/nvim-web-devicons" },
+    })
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{
-				"williamboman/mason.nvim",
-				run = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" },
-			{ "jose-elias-alvarez/null-ls.nvim" },
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
+        requires = {
+            -- LSP Support
+            {
+                "williamboman/mason.nvim",
+                run = function()
+                    pcall(vim.cmd, "MasonUpdate")
+                end,
+            },
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason-lspconfig.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
-		},
-	})
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
+        },
+    })
 
-	use({
-		"VonHeikemen/searchbox.nvim",
-		requires = {
-			{ "MunifTanjim/nui.nvim" },
-		},
-	})
+    use({
+        "jay-babu/mason-null-ls.nvim",
+        -- event = { "BufReadPre", "BufNewFile" },
+        requires = {
+            { "williamboman/mason.nvim" },
+            { "jose-elias-alvarez/null-ls.nvim" },
+        },
+    })
 
-	use("mbbill/undotree")
+    use({
+        "VonHeikemen/searchbox.nvim",
+        requires = {
+            { "MunifTanjim/nui.nvim" },
+        },
+    })
 
-	-- Easily move lines
-	use("fedepujol/move.nvim")
+    use("mbbill/undotree")
 
-	-- Closes brackets
-	use("rstacruz/vim-closer")
+    -- Easily move lines
+    use("fedepujol/move.nvim")
 
-	-- Preview colors
-	use("ap/vim-css-color")
+    -- Closes brackets
+    use("rstacruz/vim-closer")
 
-	-- Adds indentation guides
-	use("lukas-reineke/indent-blankline.nvim")
+    -- Preview colors
+    use("ap/vim-css-color")
 
-	use("mhartington/formatter.nvim")
+    -- Adds indentation guides
+    use("lukas-reineke/indent-blankline.nvim")
 
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+    if packer_bootstrap then
+        require("packer").sync()
+    end
 end)
