172ee3f4aaa9fa1663a457e8966071dee246d59b
nktfh100 <me@malachi.io>
2025-04-12 00:15:40 +0300

    Add missing lspconfig servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -106,6 +106,15 @@ return {
 					quiet = false,
 				},
 			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jsonls.setup({
+				capabilities = capabilities,
+			})
 			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
