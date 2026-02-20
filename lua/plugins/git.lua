7c9b051292645d9a5d61e02955d4fd2e3c5cadf9
nktfh100 <me@malachi.io>
2026-02-21 00:33:02 +0200

    add gitsigns.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -21,6 +21,43 @@ return {
 			-- },
 		},
 	},
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns-opts`
+			signs = {
+				add = { text = "▎" },
+				change = { text = "▎" },
+				delete = { text = "" },
+				topdelete = { text = "―" },
+				changedelete = { text = "▎" },
+			},
+			on_attach = function(bufnr)
+				local gs = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]h", gs.next_hunk, { desc = "Next Hunk" })
+				map("n", "[h", gs.prev_hunk, { desc = "Prev Hunk" })
+
+				-- Actions
+				map("n", "<leader>ga", gs.stage_hunk, { desc = "Stage Hunk" })
+				map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset Hunk" })
+				map("n", "<leader>gR", gs.reset_buffer, { desc = "Reset Buffer" })
+				map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo Stage Hunk" })
+				map("n", "<leader>gw", gs.toggle_deleted, { desc = "Toggle Deleted" })
+				map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview Hunk" })
+				map("n", "<leader>gL", function() gs.blame_line({ full = true }) end, { desc = "Blame Line" })
+				map("n", "<leader>gD", gs.diffthis, { desc = "Diff This" })
+				map("n", "<leader>ub", function() gs.setup({ current_line_blame = true }) end, { desc = "Toggle Line Blame" })
+			end,
+		},
+	},
 	-- Git commands
 	{
 		"tpope/vim-fugitive",
